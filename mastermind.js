5df5919924c5346f91132e614e597f06cb730713
Sonni <83771204+sonni46@users.noreply.github.com>
2026-04-10 19:55:49 +0200

    restructured the code and added alert game

--- mastermind.js
+++ mastermind.js
@@ -21,12 +21,26 @@ function random_numbers() {
   } 
 }
 function colors_randomizer() {
-  random_numbers()
   for(const COLOR of _NUMS_WINNING_) {
     _WINNING_COLORS_.push(COLORS[COLOR])
   }
 }
-function get_pins_containerss() {
+
+function new_game() {  
+  random_numbers()
+  colors_randomizer()
+}
+
+function allert_game(txt) {  
+  let feed_cont = document.getElementById("feedback_container") 
+  if (feed_cont.innerText.length > 0) return
+  feed_cont.innerText = txt
+  setTimeout(() => {
+    feed_cont.innerText = ""
+  },2000)
+}
+
+function get_pins_containers() {
   // cerca i contenitori dei pin che non hanno lo stato "complete"
   let pins_containers = document.getElementById("try_board").querySelectorAll(".pins_left")
   pins_containers = Array.from(pins_containers).filter(cont_pin => !cont_pin.classList.contains("complete"))
@@ -34,7 +48,7 @@ function get_pins_containerss() {
 }
 function select_color(el) {
   let color = el.getAttribute("data-color");
-  let pins_containers = get_pins_containerss();
+  let pins_containers = get_pins_containers();
   let len_pins_containers = pins_containers.length - 1;
   if (len_pins_containers === -1) return
   // cerca nella coda tutti i nodi che non hanno data-color
@@ -42,13 +56,13 @@ function select_color(el) {
   //cerca se hai selezionato lo stesso colore
   let is_color_used = _selected_colors_.includes(color)
 
-  if (!pins_not_color.length) return; // XXX fare un errore es."hai superato il limite di colori"
-  if (is_color_used) return; // XXX fare un errore es."stai usando lo stesso colore"
+  if (!pins_not_color.length) return allert_game("Limit reached"); 
+  if (is_color_used) return allert_game("Color already used"); // XXX fare un errore es."stai usando lo stesso colore"
   pins_not_color[0].setAttribute("data-color", color); // render color 
   _selected_colors_.push(color)
 }
 function delete_color() {
-  let pins_containers = get_pins_containerss()
+  let pins_containers = get_pins_containers()
   let end_pins = pins_containers.length - 1;
   if (end_pins === -1) return;
   let pins_containers_tail = pins_containers[end_pins].querySelectorAll("[data-color]");
@@ -59,26 +73,15 @@ function delete_color() {
 }
 function reset_game() {
   document.querySelector(".board").innerHTML = _board_
+  allert_game("New game loaded")
   _selected_colors_ = []
+  new_game()
 }
-function init_game() {
-  colors_randomizer()
-}
-// function check_color() {
-//   let pins_containers = get_pins_containerss()
-//   let len = pins_containers.length - 1
-//   if (len === -1) return
-//   let pins_containers_tail = pins_containers[len]
-//   let pins_colors = pins_containers_tail.querySelectorAll("[data-color]") 
-//   if (pins_colors.length === 4) pins_containers_tail.classList.add("complete")
-//   let colors = Array.from(pins_colors).map(el => el.getAttribute("data-color"))
-//   // XXX fare il chck dei colori de delle posizioni 
-// }
 function is_found_combination() {
   // XXX fare algoritmo per la verifica dei colori se sono posizionati correttamente
 }
 function check_color() {
-  let pins_containers = get_pins_containerss()
+  let pins_containers = get_pins_containers()
   let len = pins_containers.length - 1
   if (len === -1) return
 
@@ -91,6 +94,10 @@ function check_color() {
   // XXX mostrare il risultato in caso di successo
   if (is_found_combination()) true
   else _selected_colors_ = []
+}
+
+function init_game() {
+  new_game()
 }
 
 document.addEventListener("DOMContentLoaded", () => {
